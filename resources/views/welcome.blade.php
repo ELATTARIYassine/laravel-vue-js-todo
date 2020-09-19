0d5337a18a60146bd26dd6e80468935cbcb0bbd6
Yassine EL ATTARI <yassineattari64@gmail.com>
2020-09-19 14:56:55 +0000

    preparing UI

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="w-100 h-100 d-flex flex-column justify-content-center align-items-center">
+        <h1 class="text-white display-4">TODO Vue js & Laravel</h1>
+    </div>  
+@endsection
